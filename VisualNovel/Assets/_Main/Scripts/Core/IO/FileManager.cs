f74454ee4f74b55842113c9e5114d1ec09cdf227
kalderlunt <kalderlunt@gmail.com>
2024-03-26 16:40:58 +0100

    reading text files
    add script :
    - TestFiles.cs
    - FilesPath.cs
    - FileManager.cs
    
    add file:
    - testFiles.txt

---
+++ VisualNovel/Assets/_Main/Scripts/Core/IO/FileManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileManager 
+{
+    public static List<string> ReadTextFile(string filePath, bool includeBlankLines = true)
+    {
+        if (!filePath.StartsWith('/'))
+            filePath = FilePaths.root + filePath;
+
+        List<string> lines = new();
+        try
+        {
+            using (StreamReader sr = new(filePath)) 
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (includeBlankLines || !string.IsNullOrWhiteSpace(line))
+                        lines.Add(line);
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Debug.LogError($"File not found : '{ex.FileName}'");
+        }
+
+        return lines;
+    }
+
+    public static List<string> ReadTextAsset(string filePath, bool includeBlankLines = true)
+    {
+        TextAsset asset = Resources.Load<TextAsset>(filePath);
+        if (asset == null)
+        {
+            Debug.LogError($"Asset not found : '{filePath}'");
+            return null;
+        }
+        
+        return ReadTextAsset(asset, includeBlankLines);
+    }
+
+    public static List<string> ReadTextAsset(TextAsset asset, bool includeBlankLines = true)
+    {
+        List<string> lines = new();
+        using (StringReader sr = new(asset.text))
+        {
+            while (sr.Peek() > -1)
+            {
+                string line = sr.ReadLine();
+                if (includeBlankLines || !string.IsNullOrWhiteSpace(line))
+                    lines.Add(line);
+            }
+        }
+        return lines;
+    }
+}
